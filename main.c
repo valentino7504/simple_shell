30943d43cea49484555a0a2d15f0db1b0467a61e
Edwin Ade <edwinmbonyjr@gmail.com>
2023-05-26 20:14:13 +0100

    fixed spaces issue

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int main(__attribute__((unused)) int argc, char *argv[])
 			free(user_entry);
 			exit(status);
 		}
-		if (strspn(user_entry, " \n") == _strlen(user_entry))
+		if (strspn(user_entry, " \n") == (size_t) _strlen(user_entry))
 		{
 			free(user_entry);
 			continue;
